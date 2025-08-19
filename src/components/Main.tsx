e7ca0766343ba8e207048f78eb9403369c9be958
Renat <restspread@gmail.com>
2025-08-19 23:02:37 +0300

    add input, iframe

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Main() {
 	return (
-		<div className='bg-green-200 text-black flex-5/6'>Content</div>
+		<div className='bg-green-200 text-black flex-5/6'>
+			<input className='bg-white' type="text" />
+			<iframe
+				src="https://kg-portal.ru/"
+				title="Example Iframe"
+				className="w-100 h-200"
+				style={{ border: 'none' }}
+			></iframe>
+		</div>
 	)
 }
