255ddaf25deec1ac40dd2c5b4cdcb5b8575664d9
Renat <restspread@gmail.com>
2025-08-19 20:26:18 +0300

    first commit

---
+++ src/app/components/Main.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Main() {
+	return (
+		<div className='bg-green-200 text-black flex-5/6'></div>
+	)
+}
---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Sidebar() {
+	return (
+		<div className='bg-red-200 text-black flex-1/6 p-2'>Sidebar</div>
+	)
+}
