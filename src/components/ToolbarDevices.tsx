c66782bc7b886843bbb86d6c12f1bedcd86321ed
Renat <restspread@gmail.com>
2025-08-23 00:20:55 +0300

    add next-intl

--- src/components/ToolbarDevices.tsx
+++ src/components/ToolbarDevices.tsx
@@ -1,12 +1,14 @@
+import { useTranslations } from 'next-intl';
 import Image from "next/image";
 import iconDesk from '@/images/i-desktop.png';
 import iconTab from '@/images/i-tablet.png';
 import iconMob from '@/images/i-mobile.png';
 
 export default function ToolbarDevices() {
+	const t = useTranslations('ToolbarDevices');
 	return (
 		<div>
-			<h3 className="mb-1">Devices</h3>
+			<h3 className="mb-1">{t('devices')}</h3>
 			<div className="flex justify-around items-center border-1 rounded-sm p-1">
 				<button className="cursor-pointer">
 					<Image src={iconMob} alt="" width={20} height={32} />
