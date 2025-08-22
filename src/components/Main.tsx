a60c4f22e5a9a527d2faf6806e8c15f6445aa52b
Renat <restspread@gmail.com>
2025-08-22 21:12:25 +0300

    add comp

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,14 +2,21 @@ import React from 'react'
 
 export default function Main() {
 	return (
-		<div className='bg-green-200 text-black flex-5/6'>
-			<input className='bg-white' type="text" />
-			<iframe
-				src="https://kg-portal.ru/"
-				title="Example Iframe"
-				className="w-100 h-200"
-				style={{ border: 'none' }}
-			></iframe>
+		<div className='bg-green-200 text-black flex-5/6 p-2'>
+			<div className='mb-4'>
+				<input className='bg-white min-w-[600px] p-2' type="text" />
+				<button className='bg-blue-500 text-white p-2'>Search</button>
+			</div>
+			{/* devices */}
+			<div className='flex flex-wrap gap-4'>
+				<iframe
+					src="http://localhost:3000/"
+					title="Example Iframe"
+					className="w-[320px] h-[568px]"
+					style={{ border: 'none' }}
+				></iframe>
+
+			</div>
 		</div>
 	)
 }
